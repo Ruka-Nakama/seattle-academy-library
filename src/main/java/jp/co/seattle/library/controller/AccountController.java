d76dc19a9b39f2a193c093978c8249f5601aed87
rukanakama <rukanakama@192.168.130.168>
2023-04-11 12:47:12 +0900

    task1三次コミット

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -48,8 +48,9 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-		if(password.length()>=8&&password.matches("[A-Za-z0-9]")) {
-			if(password == passwordForCheck) {
+		if(password.length()>=8&&password.matches("[A-Za-z0-9]+")) {
+			if(password.equals("passwordForCheck")) {
+				// パラメータで受け取ったアカウント情報をDtoに格納する。
 				UserInfo userInfo = new UserInfo();
 				userInfo.setEmail(email);
 				userInfo.setPassword(password);
@@ -63,7 +64,7 @@ public class AccountController {
 			model.addAttribute("errorMessage", "パスワードは8文字以上かつ半角英数字に設定してください");
 			return "createAccount";
 		}
-		// パラメータで受け取ったアカウント情報をDtoに格納する。
+		
 		
 	}
 		
